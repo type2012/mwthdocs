8ab99578953f98b13d6660ec457aa8167d0bf252
choknt <153351426+choknt@users.noreply.github.com>
2025-02-14 21:39:54 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            ไปเริ่มเรียนรู้กันเลย ⏱️
           </Link>
         </div>
       </div>
