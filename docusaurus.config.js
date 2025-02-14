8464ad454a3260f46c0b9f286d62af2f39292fc3
choknt <153351426+choknt@users.noreply.github.com>
2025-02-14 21:03:49 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'mwth',
-  tagline: 'mw Thai dosc',
+  tagline: 'MW Thai dosc',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
